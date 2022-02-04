5fa42e7dde9d934e0679d73c74daf1465e4a2b48
Nick Rusin <woundculture@gmail.com>
2022-02-04 05:10:31 +0000

    exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Carl", last_name: "Sagan", hourly_rate: 420)
+@store1.employees.create(first_name: "Frank", last_name: "Zappa", hourly_rate: 300)
+@store2.employees.create(first_name: "Rivers", last_name: "Cuomo", hourly_rate: 20)
+@store2.employees.create(first_name: "Ozzy", last_name: "Osborne", hourly_rate: 666)
+
+@store1.employees.each do |s|
+  puts "#{s.first_name} #{s.last_name}"
+end
+
+@store2.employees.each do |s|
+  puts "#{s.first_name} #{s.last_name}"
+end
